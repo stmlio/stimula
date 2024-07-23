6991bde2cf6fbae2224399702efd1dc304b8cca2
Romke Jonker <romke@jnkr.nl>
2024-07-23 17:50:12 +0200

    Set version to 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stimula',
-    version='0.0.4',
+    version='0.0.5',
     description='Toolset library for Simple Table Mapping Language (STML)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
