208fe029da75839b4771ae9d97bd25accf0373e1
Romke Jonker <romke@jnkr.nl>
2024-10-15 10:15:38 +0200

    Open range for crypto dependency for odoo:18 compatibility. Version 1.1.2

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='stimula',
-    version='1.1.1',
+    version='1.1.2',
     description='Toolset library for Simple Table Mapping Language (STML)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/stmlio/stimula',
     author='Romke Jonker',
-    author_email='romke@rnadesign.net',
+    author_email='romke@stml.io',
     packages=find_packages(),
     install_requires=[
         'pyjwt',
         'sqlalchemy',
         'pandas>=2.1.3',
         'numpy>=1.22.4',
-        'cryptography>=3.4.8,<4.0',
+        'cryptography>=3.4.8',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
